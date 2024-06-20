91227162c7ebd130e45e999586bd72831a3a6eef
aldair <torrezaldair777@gmail.com>
2024-06-20 12:50:06 -0400

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        shark: {
+          50: "#f6f6f6",
+          100: "#e7e7e7",
+          200: "#d1d1d1",
+          300: "#b0b0b0",
+          400: "#888888",
+          500: "#6d6d6d",
+          600: "#5d5d5d",
+          700: "#4f4f4f",
+          800: "#454545",
+          900: "#3d3d3d",
+          950: "#1e1e1e",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
