75b703c6683be5c7ce94853abe5d2961e4fe3e55
aldair <torrezaldair777@gmail.com>
2026-03-19 09:00:49 -0400

    3 subdomains

--- internal/server/static/applicant-playback.js
+++ internal/server/static/applicant-playback.js
@@ -1,7 +1,7 @@
 // Applicant Recording Playback - Button-triggered loading for offerAdmin page
 // Adapted from playback.js for multiple applicants with on-demand loading
 (function () {
-  const BASE = "https://mediamtx.menudencia.site/playback"
+  const BASE = "https://playback.menudencia.site"
 
     function buildGetUrl(path, start, duration) {
         const url = new URL('/get', BASE);
