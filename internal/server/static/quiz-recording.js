0c852744369c8011e5982a4fde4c0529ec113a51
aldair <torrezaldair777@gmail.com>
2026-03-19 09:13:26 -0400

    http only

--- internal/server/static/quiz-recording.js
+++ internal/server/static/quiz-recording.js
@@ -5,7 +5,7 @@
     const sessionID = window.participationID;
 
     //const BASE_URL = 'http://localhost:8889/';
-    const BASE_URL = "https://recordings.menudencia.site/";
+    const BASE_URL = "http://recordings.menudencia.site/";
 
     let pc = null;
     let localStream = null;
